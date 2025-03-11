7e3e21fc01e3b23ab0cd48bfb4a93772dc3d44d9
rafin8990 <rafin8990@gmail.com>
2025-03-11 16:21:50 +0600

    updated

--- src/app/modules/poll/poll.service.ts
+++ src/app/modules/poll/poll.service.ts
@@ -3,7 +3,6 @@ import { v4 as uuidv4 } from 'uuid'
 import ApiError from '../../../errors/ApiError'
 import { IPoll } from './poll.interface'
 import { Poll } from './poll.model'
-import { isValidObjectId } from 'mongoose'
 
 const createPoll = async (poll: IPoll): Promise<IPoll> => {
   poll.uuid = uuidv4()
@@ -34,84 +33,103 @@ const getPollById = async (userId: string, pollId: string): Promise<IPoll> => {
   return poll
 }
 
-const voteForOption = async (pollId: string, userId: string, option: string): Promise<IPoll> => {
-  const poll = await Poll.findById(pollId);
+const voteForOption = async (
+  pollId: string,
+  userId: string,
+  option: string
+): Promise<IPoll> => {
+  const poll = await Poll.findById(pollId)
   if (!poll) {
-    throw new ApiError(httpStatus.NOT_FOUND, 'Poll not found');
+    throw new ApiError(httpStatus.NOT_FOUND, 'Poll not found')
   }
 
   if (!poll.options.includes(option)) {
-    throw new ApiError(httpStatus.BAD_REQUEST, 'Invalid option selected');
+    throw new ApiError(httpStatus.BAD_REQUEST, 'Invalid option selected')
   }
 
   if (poll.voters.has(userId)) {
-    const previousVote = poll.voters.get(userId);
+    const previousVote = poll.voters.get(userId)
 
     if (previousVote === option) {
-      throw new ApiError(httpStatus.BAD_REQUEST, 'You have already voted for this option');
+      throw new ApiError(
+        httpStatus.BAD_REQUEST,
+        'You have already voted for this option'
+      )
     } else if (previousVote) {
-      const previousVoteCount = poll.votes[previousVote] ?? 0;  
-      poll.votes[previousVote] = previousVoteCount - 1;
+      const previousVoteCount = poll.votes[previousVote] ?? 0
+      poll.votes[previousVote] = previousVoteCount - 1
     }
   }
 
-  const currentVoteCount = poll.votes[option] ?? 0;  
-  poll.votes[option] = currentVoteCount + 1;
-  poll.voters.set(userId, option);
+  const currentVoteCount = poll.votes[option] ?? 0
+  poll.votes[option] = currentVoteCount + 1
+  poll.voters.set(userId, option)
 
-  await poll.save();
-  return poll;
-};
+  await poll.save()
+  return poll
+}
 
-const addCommentToPoll = async (pollId: string, commentText: string): Promise<IPoll> => {
-  const poll = await Poll.findById(pollId);
+const addCommentToPoll = async (
+  pollId: string,
+  commentText: string
+): Promise<IPoll> => {
+  const poll = await Poll.findById(pollId)
   if (!poll) {
-    throw new ApiError(httpStatus.NOT_FOUND, 'Poll not found');
+    throw new ApiError(httpStatus.NOT_FOUND, 'Poll not found')
   }
 
   poll.comments.push({
     text: commentText,
     createdAt: new Date(),
-  });
+  })
 
-  await poll.save();
-  return poll;
-};
+  await poll.save()
+  return poll
+}
 
-const addReactionToPoll = async (pollId: string, userId: string, reaction: string): Promise<IPoll> => {
-  const poll = await Poll.findById(pollId);
+const addReactionToPoll = async (
+  pollId: string,
+  userId: string,
+  reaction: string
+): Promise<IPoll> => {
+  const poll = await Poll.findById(pollId)
   if (!poll) {
-    throw new ApiError(httpStatus.NOT_FOUND, 'Poll not found');
+    throw new ApiError(httpStatus.NOT_FOUND, 'Poll not found')
   }
 
-  const validReactions = ['Trending', 'Like'];
+  const validReactions = ['Trending', 'Like']
   if (!validReactions.includes(reaction)) {
-    throw new ApiError(httpStatus.BAD_REQUEST, 'Invalid reaction');
+    throw new ApiError(httpStatus.BAD_REQUEST, 'Invalid reaction')
   }
 
   if (poll.reactions.has(userId)) {
-    throw new ApiError(httpStatus.BAD_REQUEST, 'You have already reacted to this poll');
+    throw new ApiError(
+      httpStatus.BAD_REQUEST,
+      'You have already reacted to this poll'
+    )
   }
 
-  const reactionCounts = new Map<string, number>(Object.entries(poll.reactionCounts));
-  const currentReactionCount = reactionCounts.get(reaction) ?? 0;
-  reactionCounts.set(reaction, currentReactionCount + 1);
-  poll.reactionCounts = Object.fromEntries(reactionCounts);
-  await poll.save();
+  const reactionCounts = new Map<string, number>(
+    Object.entries(poll.reactionCounts)
+  )
+  const currentReactionCount = reactionCounts.get(reaction) ?? 0
+  reactionCounts.set(reaction, currentReactionCount + 1)
+  poll.reactionCounts = Object.fromEntries(reactionCounts)
+  await poll.save()
 
-  return poll;
-};
+  return poll
+}
 
-const getReactionCounts = async (pollId: string): Promise<Record<string, number>> => {
-  const poll = await Poll.findById(pollId);
+const getReactionCounts = async (
+  pollId: string
+): Promise<Record<string, number>> => {
+  const poll = await Poll.findById(pollId)
   if (!poll) {
-    throw new ApiError(httpStatus.NOT_FOUND, 'Poll not found');
+    throw new ApiError(httpStatus.NOT_FOUND, 'Poll not found')
   }
 
-  return poll.reactionCounts;
-};
-
- 
+  return poll.reactionCounts
+}
 
 export const PollService = {
   createPoll,
@@ -119,5 +137,5 @@ export const PollService = {
   voteForOption,
   addCommentToPoll,
   addReactionToPoll,
-  getReactionCounts
+  getReactionCounts,
 }
